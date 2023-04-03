7a0c5379e4ebc57c44568dfbb9d9837540e00cc0
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-04-04 00:34:39 +0100

    Feat: Added configuration with puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,23 @@
+# configures a webserver
+
+package {'nginx':
+ensure   => 'installed',
+provider => 'apt'
+}
+
+exec {'100 allow nginx':
+command => '/usr/sbin/ufw allow "Nginx HTTP"'
+}
+
+
+file_line { 'addheader':
+  ensure => 'present',
+  path   => '/etc/nginx/nginx.conf',
+  after  => 'http {',
+  line   => "	add_header X-Served-By ${hostname};",
+}
+
+service {'nginx':
+ensure => running,
+enable => true
+}
