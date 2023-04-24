529ce1a6e0c418760e91138acdd0cb33e3299c08
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-04-24 17:12:00 +0100

    Feat: Added 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,13 +3,13 @@
 import requests
 import sys
 
-if len(sys.argv) > 1 and type(eval(sys.argv[1])) == int:
+if len(sys.argv) > 1 and isinstance(eval(sys.argv[1]), int):
     pass
 else:
     sys.exit(0)
 
 BASE_API = "https://jsonplaceholder.typicode.com/"
-employee_id = sys.argv[1];
+employee_id = sys.argv[1]
 user_response_url = BASE_API + "users/{}".format(employee_id)
 todo_response_url = BASE_API + "users/{}/todos".format(employee_id)
 
@@ -20,16 +20,17 @@ employee_name = user_response.get('name')
 
 num_tasks_done = 0
 for todo in todo_response:
-    if todo.get("completed") == True:
+    if todo.get("completed"):
         num_tasks_done += 1
 
 total_num_tasks = 0
 for todo in todo_response:
     total_num_tasks += 1
 
-print("Employee {} is done with tasks({}/{}):".format(employee_name, num_tasks_done, total_num_tasks))
+print("Employee {} is done with tasks({}/{}):".format(employee_name,
+      num_tasks_done, total_num_tasks))
 
 for todo in todo_response:
     task_title = todo.get("title")
-    if (todo.get("completed") == True):
+    if (todo.get("completed")):
         print("     {}".format(task_title))
