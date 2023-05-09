2921d9197c22fd4687880e18b0302ce911396f54
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-05-09 16:35:17 +0100

    Added 2-recurse.py

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Advanced Apis Module"""
+import requests
+import sys
+
+
+def recurse(subreddit, hot_list=[], after=None,):
+    """function that queries the Reddit API and prints
+    the titles of the first 10 hot posts listed for a given subreddit."""
+
+    headers = {"User-Agent": "user_agent"}
+    url = "https://www.reddit.com/r/{}/hot.json".format(
+        subreddit)
+    params = {"limit": 100, "after": after}
+
+    response = requests.get(
+        url=url,
+        headers=headers,
+        params=params,
+        allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()['data']
+        children = data['children']
+        hot_list.append([child['data']['title']
+                         for child in children])
+        after = response.json()['data']['after']
+        if not after:
+            return hot_list
+        return recurse(subreddit, hot_list, after)
+    else:
+        return None
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        subred = sys.argv[1]
+        recurse(subred, [])
